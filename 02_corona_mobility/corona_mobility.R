0a563c10378c0a03480f7bcc754b500e7e995912
Simon Grundt Straubinger <simon.wismar@gmail.com>
2020-05-17 19:48:38 +0200

    Update corona dataviz

--- 02_corona_mobility/corona_mobility.R
+++ 02_corona_mobility/corona_mobility.R
@@ -1,6 +1,7 @@
 library(tidyverse)
 library(covdata)
 library(extrafont)
+library(ggtext)
 
 # The Google and Apple mobility data is importet from the covdata package by Kieran Healy
 # remotes::install_github("kjhealy/covdata")
@@ -8,11 +9,9 @@ library(extrafont)
 # The plots use the font Roboto, download the font from https://fonts.google.com/
 # After installation of the font execute this command to import the font: font_import()
 
-# Google Community Mobility Reports, Denmark ------------------------------
+theme_set(theme_minimal())
 
-ann1 <- data.frame(date = as.Date("2020-03-10"), pct_diff = 1.6, type = "Grocery")
-ann2 <- data.frame(date = as.Date("2020-03-18"), pct_diff = 1.6, type = "Grocery")
-ann3 <- data.frame(date = as.Date("2020-04-05"), pct_diff = 0.6, type = "Grocery")
+# Google Community Mobility Reports, Denmark ------------------------------
 
 google_mobility %>% 
   filter(country_region == "Denmark", is.na(sub_region_1)) %>% 
@@ -20,26 +19,23 @@ google_mobility %>%
          date = as.Date(date),
          pct_diff = pct_diff/100) %>%
   ggplot(aes(x = date, y = pct_diff)) +
-  geom_ribbon(aes(ymin=pmin(pct_diff,0), ymax=0), fill="#CC1F1A", alpha=0.8) +
-  geom_ribbon(aes(ymin=0, ymax=pmax(pct_diff,0)), fill="#2779BD", alpha=0.8) +
+  geom_ribbon(aes(ymin=pmin(pct_diff,0), ymax=0), fill="#CC1F1A", alpha=0.7) +
+  geom_ribbon(aes(ymin=0, ymax=pmax(pct_diff,0)), fill="#2779BD", alpha=0.7) +
   geom_hline(yintercept = 0) +
-  geom_vline(xintercept = as.Date("2020-03-11"), size = 0.3, linetype = "dashed", color = "#3D4852") +
-  geom_vline(xintercept = as.Date("2020-03-17"), size = 0.3, linetype = "dashed", color = "#3D4852") +
-  geom_vline(xintercept = as.Date("2020-04-06"), size = 0.3, linetype = "dashed", color = "#3D4852") +
-  geom_text(data = ann1, family = "Roboto", size = 2.5, hjust = "right",
-            label = "Pressemøde\n11. marts") +
-  geom_text(data = ann2, family = "Roboto", size = 2.5, hjust = "left",
-            label = "Pressemøde\n17. marts") +
-  geom_text(data = ann3, family = "Roboto", size = 2.5, hjust = "right",
-            label = "Pressemøde\n6. april") +
+  geom_vline(xintercept = as.Date("2020-03-11"), size = 0.4, linetype = "dashed", color = "#CC1F1A") +
+  geom_vline(xintercept = as.Date("2020-03-17"), size = 0.4, linetype = "dashed", color = "#CC1F1A") +
+  geom_vline(xintercept = as.Date("2020-04-06"), size = 0.4, linetype = "dashed", color = "#1F9D55") +
+  geom_vline(xintercept = as.Date("2020-05-07"), size = 0.4, linetype = "dashed", color = "#1F9D55") +
   scale_x_date(date_labels = "%d. %b") +
   scale_y_continuous(labels = scales::percent) +
   facet_wrap(~ type) +
-  theme_minimal() +
   labs(title = "Mobilitet i Danmark under COVID-19",
-       subtitle = "Procentuel difference i mobilitet ift. median opgjort for perioden 3. januar - 6. februar 2020",
+       subtitle = "Procentuel difference i mobilitet ift. median opgjort for perioden 3. januar til 6. februar 2020.<br/>
+       Stiplede linjer markerer pressemøder om <span style='color:#CC1F1A'>nedlukning</span> og 
+       <span style='color:#1F9D55'>genåbning</span> af Danmark.",
        caption = "@StraubingerDK | Data: Google Community Mobility Reports") +
   theme(plot.title = element_text(face = "bold"),
+        plot.subtitle = element_markdown(lineheight = 1.2),
         legend.position = "none",
         plot.caption = element_text(color = "#606F7B", margin = margin(t = 10)),
         axis.title = element_blank(),
@@ -48,14 +44,10 @@ google_mobility %>%
         panel.spacing.x = unit(2, "lines"),
         panel.spacing.y = unit(1, "lines"))
 
-ggsave(width = 8, height = 4, "plot_google_mobility.png")
+ggsave(width = 9, height = 5, "plot_google_mobility.png")
 
 
 # Apple Mobility Trends Reports, Denmark ----------------------------------
-
-ann4 <- data.frame(date = as.Date("2020-03-10"), index = 0.4, transportation_type = "Driving")
-ann5 <- data.frame(date = as.Date("2020-03-18"), index = 0.4, transportation_type = "Driving")
-ann6 <- data.frame(date = as.Date("2020-04-05"), index = 0.2, transportation_type = "Driving")
 
 apple_mobility %>% 
   filter(region == "Denmark") %>% 
@@ -66,23 +58,21 @@ apple_mobility %>%
   geom_ribbon(aes(ymin=pmin(index,0), ymax=0), fill="#CC1F1A", alpha=0.8) +
   geom_ribbon(aes(ymin=0, ymax=pmax(index,0)), fill="#2779BD", alpha=0.8) +
   geom_hline(yintercept = 0) +
-  geom_vline(xintercept = as.Date("2020-03-11"), size = 0.3, linetype = "dashed", color = "#3D4852") +
-  geom_vline(xintercept = as.Date("2020-03-17"), size = 0.3, linetype = "dashed", color = "#3D4852") +
-  geom_vline(xintercept = as.Date("2020-04-06"), size = 0.3, linetype = "dashed", color = "#3D4852") +
-  geom_text(data = ann4, family = "Roboto", size = 2.5, hjust = "right",
-            label = "Pressemøde\n11. marts") +
-  geom_text(data = ann5, family = "Roboto", size = 2.5, hjust = "left",
-            label = "Pressemøde\n17. marts") +
-  geom_text(data = ann6, family = "Roboto", size = 2.5, hjust = "right",
-            label = "Pressemøde\n6. april") +
+  geom_vline(xintercept = as.Date("2020-03-11"), size = 0.4, linetype = "dashed", color = "#CC1F1A") +
+  geom_vline(xintercept = as.Date("2020-03-17"), size = 0.4, linetype = "dashed", color = "#CC1F1A") +
+  geom_vline(xintercept = as.Date("2020-04-06"), size = 0.4, linetype = "dashed", color = "#1F9D55") +
+  geom_vline(xintercept = as.Date("2020-05-07"), size = 0.4, linetype = "dashed", color = "#1F9D55") +
   scale_x_date(date_labels = "%d. %b") +
   scale_y_continuous(labels = scales::percent) +
   facet_wrap(~ transportation_type) +
   labs(title = "Mobilitet i Danmark under COVID-19",
-       subtitle = "Procentuel difference i mobilitet ift. den 13. januar 2020",
+       subtitle = "Procentuel difference i mobilitet ift. den 13. januar 2020.<br/>
+       Stiplede linjer markerer pressemøder om <span style='color:#CC1F1A'>nedlukning</span> og 
+       <span style='color:#1F9D55'>genåbning</span> af Danmark",
        caption = "@StraubingerDK | Data: Apple Mobility Trends Reports") +
   theme_minimal() +
   theme(plot.title = element_text(face = "bold"),
+        plot.subtitle = element_markdown(lineheight = 1.2),
         legend.position = "none",
         plot.caption = element_text(color = "#606F7B", margin = margin(t = 10)),
         axis.title = element_blank(),
@@ -90,7 +80,7 @@ apple_mobility %>%
         strip.text=element_text(face = "bold", hjust = 0),
         panel.spacing.x = unit(2, "lines"))
 
-ggsave(width = 8, height = 4, "plot_apple_mobility.png")
+ggsave(width = 9, height = 5, "plot_apple_mobility.png")
 
 
 # Google Community Mobility Reports, Danish regions -----------------------
@@ -100,26 +90,31 @@ google_mobility %>%
   mutate(type = tools::toTitleCase(type),
          date = as.Date(date),
          pct_diff = pct_diff/100,
-         sub_region_1 = case_when(sub_region_1 == "Capital Region of Denmark" ~ "Hovedstaden",
-                                  sub_region_1 == "Central Denmark Region" ~ "Midtjylland",
-                                  sub_region_1 == "North Denmark Region" ~ "Nordjylland",
-                                  sub_region_1 == "Region of Southern Denmark" ~ "Syddanmark",
-                                  sub_region_1 == "Region Zealand" ~ "Sjælland")
+         sub_region_1 = recode_factor(sub_region_1,
+                                      "North Denmark Region" = "Nordjylland",
+                                      "Central Denmark Region" = "Midtjylland",
+                                      "Region of Southern Denmark" = "Syddanmark",
+                                      "Region Zealand" = "Sjælland",
+                                      "Capital Region of Denmark" = "Hovedstaden")
   ) %>% 
   ggplot(aes(x = date, y = sub_region_1, fill = pct_diff)) +
   geom_tile() +
-  geom_vline(xintercept = as.Date("2020-03-11"), size = 0.4, linetype = "dashed", color = "#3D4852") +
-  geom_vline(xintercept = as.Date("2020-03-17"), size = 0.4, linetype = "dashed", color = "#3D4852") +
-  geom_vline(xintercept = as.Date("2020-04-06"), size = 0.4, linetype = "dashed", color = "#3D4852") +
+  geom_vline(xintercept = as.Date("2020-03-11"), size = 0.4, linetype = "dashed", color = "#CC1F1A") +
+  geom_vline(xintercept = as.Date("2020-03-17"), size = 0.4, linetype = "dashed", color = "#CC1F1A") +
+  geom_vline(xintercept = as.Date("2020-04-06"), size = 0.4, linetype = "dashed", color = "#1F9D55") +
+  geom_vline(xintercept = as.Date("2020-05-07"), size = 0.4, linetype = "dashed", color = "#1F9D55") +
   scale_fill_gradient2(low = "#CC1F1A", high = "#2779BD",labels = scales::percent) +
   scale_x_date(date_labels = "%d. %b") +
   facet_wrap(~ type, ncol = 1) +
   labs(title = "Mobilitet i danske regioner under COVID-19",
-       subtitle = "Procentuel difference i mobilitet ift. median\nopgjort for perioden 3. januar - 6. februar 2020",
+       subtitle = "Procentuel difference i mobilitet ift. median opgjort for perioden 3. januar<br/>til 6. februar 2020.
+       Stiplede linjer markerer pressemøder om <span style='color:#CC1F1A'>nedlukning</span> og<br/>
+       <span style='color:#1F9D55'>genåbning</span> af Danmark.",
        caption = "@StraubingerDK | Data: Google Community Mobility Reports") +
   guides(fill = guide_colourbar(barwidth = 15, barheight = 0.5, ticks = FALSE)) +
   theme_minimal() +
   theme(plot.title = element_text(face = "bold"),
+        plot.subtitle = element_markdown(lineheight = 1.2),
         panel.grid = element_blank(),
         legend.position = "bottom",
         legend.title = element_blank(),
