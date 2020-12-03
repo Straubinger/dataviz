735ea95887ba26b5488da9965e4a3eab5a993220
Simon Grundt Straubinger <simon.wismar@gmail.com>
2020-12-03 17:05:16 +0100

    Update plots

--- 06_corona_municipalities/corona_municipalities.R
+++ 06_corona_municipalities/corona_municipalities.R
@@ -5,16 +5,46 @@ library(tidyverse)
 library(here)
 library(zoo)
 library(extrafont)
+library(ggrepel)
+library(patchwork)
 
 theme_set(theme_minimal())
+
+# Plot size
+aspect_ratio = 16/9
+height = 6
+width  = round(aspect_ratio*height)
 
 # The plots use the font Roboto, download the font from https://fonts.google.com/
 # After installation of the font execute this command to import the font: font_import()
 
 
+# Import classifications from Statistics Denmark --------------------------
+
+dl_csv <- read.csv2("https://www.dst.dk/klassifikationsbilag/5d18d1e0-400b-4505-92ad-6782915980a3csv_da",
+                    encoding = "UTF-8")
+
+classifications <- dl_csv %>% 
+  select(KODE, NIVEAU, TITEL) %>% 
+  group_by(gr = cumsum(NIVEAU == 1)) %>%
+  mutate(region = TITEL[NIVEAU == 1L]) %>% 
+  ungroup() %>% 
+  filter(NIVEAU == 3) %>% 
+  select(-gr) %>% 
+  left_join(dl_csv %>% 
+              select(KODE, NIVEAU, TITEL) %>% 
+              filter(NIVEAU != 1) %>% 
+              group_by(gr = cumsum(NIVEAU == 2)) %>% 
+              mutate(landsdel = TITEL[NIVEAU == 2L]) %>% 
+              ungroup() %>% 
+              filter(NIVEAU == 3) %>% 
+              select(KODE, landsdel),
+            by = "KODE")
+  
+
 # Import data from SSI ----------------------------------------------------
 
-data_date <- as.Date('2020-11-19')
+data_date <- as.Date('2020-12-03')
 
 url <- "https://covid19.ssi.dk/overvagningsdata/download-fil-med-overvaagningdata"
 
@@ -84,37 +114,32 @@ cases_plot <- cases %>%
               ungroup() %>% 
               mutate(incidens_dk = (100000/bef*cases7days)) %>% 
               select(date_incidens, incidens_dk),
-            by = "date_incidens") %>%
-  full_join(cases %>% 
-              filter(date_incidens == max(date_incidens)) %>% 
-              select(municipality,
-                     incidens_order = incidens),
-            by = "municipality") %>%
-  mutate(municipality = fct_rev(fct_reorder(as_factor(municipality), incidens_order)))
+            by = "date_incidens") %>% 
+  left_join(classifications %>% 
+              select(TITEL, region),
+            by = c("municipality" = "TITEL"))
 
 
 # Incidens line chart -----------------------------------------------------
 
-ggplot(cases_plot, aes(x = date_incidens, y = incidens)) +
+lineplot_corona <- function(region_valg) {
+cases_plot %>% 
+  filter(region == region_valg) %>% 
+ggplot(aes(x = date_incidens, y = incidens)) +
   geom_line(aes(linetype = "Kommune"), size = 0.4) +
   geom_line(aes(x = date_incidens, y = incidens_dk, linetype = "Hele DK"), size = 0.4) +
   geom_ribbon(aes(ymin = incidens, ymax = pmin(incidens, incidens_dk), fill = "Over DK-niveau", alpha = 0.05)) +
   geom_ribbon(aes(ymin = incidens_dk, ymax = pmin(incidens_dk, incidens), fill = "Under DK-niveau", alpha = 0.05)) +
   facet_wrap(~ municipality, ncol = 6) +
   scale_x_date(date_labels = "%d/%m", date_breaks = "months") +
-  scale_y_continuous(breaks = seq(0, 400, by = 200)) +
+  scale_y_continuous(breaks = seq(0, 400, by = 200), limits = c(NA, 550)) +
   scale_fill_manual(values=c("Over DK-niveau" = "#ff7a69", "Under DK-niveau" = "#559fff")) +
   scale_linetype_manual(breaks=c("Kommune","Hele DK"), values=c(1,2)) +
   guides(alpha = FALSE) +
-  labs(title = "Udvikling i COVID-19 incidens i efteråret 2020",
-       subtitle = "Incidens defineres som tilfælde COVID-19 de seneste 7 dage pr. 100.000 indbyggere",
-       caption = "@StraubingerDK | Data: SSI") +
+  labs(title = region_valg) +
   theme_minimal() +
-  theme(plot.title = element_text(face = "bold", size = 16),
-        plot.subtitle = element_text(size = 12),
-        plot.caption = element_text(color = "#606F7B", margin = margin(t = 10)),
+  theme(plot.title = element_text(size = 8, face = "bold"),
         axis.title = element_blank(),
-        axis.ticks.x = element_line(color = "#606F7B"),
         axis.text = element_text(size = 8),
         panel.grid.major.x = element_blank(),
         panel.grid.minor.x = element_blank(),
@@ -123,31 +148,118 @@ ggplot(cases_plot, aes(x = date_incidens, y = incidens)) +
         legend.title = element_blank(),
         legend.position = "bottom",
         text=element_text(family="Roboto"))
+}
+
+lineplot_corona(region_valg = "Region Hovedstaden") +
+    theme(axis.text.x = element_blank()) + 
+  lineplot_corona(region_valg = "Region Sjælland") + 
+    theme(axis.text.x = element_blank()) + 
+  lineplot_corona(region_valg = "Region Syddanmark") + 
+    theme(axis.text.x = element_blank()) + 
+  lineplot_corona(region_valg = "Region Midtjylland") + 
+    theme(axis.text.x = element_blank()) + 
+  lineplot_corona(region_valg = "Region Nordjylland") + 
+    theme(axis.ticks.x = element_line(color = "#606F7B")) +
+  plot_annotation(title = "Udvikling i COVID-19 incidens i efteråret 2020",
+                  subtitle = "Incidens defineres som tilfælde COVID-19 de seneste 7 dage pr. 100.000 indbyggere",
+                  caption = "@StraubingerDK | Data: SSI",
+                  theme = theme(plot.title = element_text(face = "bold", size = 16),
+                                plot.subtitle = element_text(size = 12),
+                                plot.caption = element_text(color = "#606F7B", margin = margin(t = 10)))) +
+  plot_layout(ncol = 1, guides = 'collect', heights = c(5, 3, 4, 4, 2)) & 
+  theme(legend.position = 'bottom',
+        text=element_text(family="Roboto"))
 
 ggsave(width = 8, height = 14, here("plot_corona_line.png"), type = "cairo-png", dpi = 600)
 
+
 # Incidens heatmap --------------------------------------------------------
 
-ggplot(cases_plot, aes(x = date_incidens, y = fct_rev(municipality), fill = incidens)) +
+heatmap_corona <- function(region_valg) {
+  cases_plot %>% 
+    filter(region == region_valg) %>% 
+ggplot(aes(x = date_incidens, y = fct_rev(municipality), fill = incidens)) +
   geom_tile() +
-  scale_fill_gradient2(low = "white", high = "#CC1F1A") +
+  scale_fill_gradient2(low = "white", high = "#CC1F1A", limits = c(NA, 550)) +
   scale_x_date(date_labels = "%d %b") +
   guides(fill = guide_colourbar(barwidth = 15, barheight = 0.5, ticks = FALSE)) +
-  labs(title = "Udvikling i COVID-19 incidens i efteråret 2020",
-       subtitle = "Incidens defineres som tilfælde af COVID-19 seneste 7 dage pr. 100.000 indbyggere",
-       caption = "@StraubingerDK | Data: SSI") +
+  labs(title = region_valg) +
   theme_minimal() +
+  theme(plot.title = element_text(size = 8, face = "bold"),
+        panel.grid = element_blank(),
+        axis.title = element_blank(),
+        legend.position = "bottom",
+        legend.title = element_blank(),
+        strip.text=element_text(hjust = 0, face = "bold"),
+        text = element_text(family = "Roboto"))
+}
+
+heatmap_corona(region_valg = "Region Hovedstaden") +
+  theme(axis.text.x = element_blank()) + 
+  heatmap_corona(region_valg = "Region Sjælland") + 
+  theme(axis.text.x = element_blank()) + 
+  heatmap_corona(region_valg = "Region Syddanmark") + 
+  theme(axis.text.x = element_blank()) + 
+  heatmap_corona(region_valg = "Region Midtjylland") + 
+  theme(axis.text.x = element_blank()) + 
+  heatmap_corona(region_valg = "Region Nordjylland") + 
+  theme(axis.ticks.x = element_line(color = "#606F7B")) +
+  plot_annotation(title = "Udvikling i COVID-19 incidens i efteråret 2020",
+                      subtitle = "Incidens defineres som tilfælde COVID-19 de seneste 7 dage pr. 100.000 indbyggere",
+                      caption = "@StraubingerDK | Data: SSI",
+                      theme = theme(plot.title = element_text(face = "bold", size = 16),
+                                    plot.subtitle = element_text(size = 12),
+                                    plot.caption = element_text(color = "#606F7B", margin = margin(t = 10)))) +
+  plot_layout(ncol = 1, guides = 'collect', heights = c(29, 17, 22, 19, 12)) & 
+  theme(legend.position = 'bottom',
+        text=element_text(family="Roboto"))
+
+ggsave(width = 8, height = 14, here("plot_corona_heat.png"), type = "cairo-png", dpi = 600)
+
+
+# Status and change of incidens scatterplot -------------------------------
+
+cases_scatterplot <- cases %>%
+  filter(date_incidens == '2020-12-01	' | date_incidens == '2020-11-24') %>% 
+  select(date_incidens, municipality, incidens, bef) %>% 
+  pivot_wider(names_from = date_incidens, values_from = incidens) %>% 
+  rename(date1 = `2020-11-24`,
+         date2 = `2020-12-01`) %>% 
+  mutate(diff = (date2-date1)/date1,
+         tema = case_when(municipality %in% c("Vallensbæk", "Rødovre", "Ishøj", "Høje-Taastrup", "Hvidovre",
+                                              "Glostrup", "Brøndby", "Albertslund", "Ballerup", "Dragør",
+                                              "Frederiksberg", "Gentofte", "Gladsaxe", "Herlev", "København", 
+                                              "Lyngby-Taarbæk", "Tårnby") ~  "kbh",
+                          municipality %in% c("Thisted", "Jammerbugt", "Hjørring", "Frederikshavn", "Læsø",
+                                              "Brønderslev", "Vesthimmerlands") ~ "nordjysk",
+                          TRUE ~ "resten")) %>% 
+  filter(is.finite(diff))
+
+ggplot(cases_scatterplot, aes(x = date2, y = diff, color = tema, alpha = 0.8)) +
+  geom_point(aes(size = bef)) +
+  geom_hline(yintercept = 0) +
+  geom_text_repel(aes(label = ifelse(diff >= 1 | date2 > 300 | date2 == 0, as.character(municipality), "")), 
+                  color = "black", family = "Roboto", size = 3.5) +
+  annotate("text", x = 270, y = -0.2, color = "#e45858", family = "Roboto", label = "Københavnsområdet") +
+  annotate("text", x = 40, y = -0.9, color = "#6246ea", family = "Roboto", label = "Nordjyske kommuner") +
+  scale_y_continuous(labels = scales::percent_format(accuracy = 1)) +
+  scale_color_manual(values = c("resten" = "gray", "kbh" = "red", "nordjysk" = "blue")) +
+  labs(title = "Status for COVID-19 incidens og seneste udvikling i danske kommuner",
+       subtitle = "Incidens defineres som tilfælde af COVID-19 seneste 7 dage pr. 100.000 indbyggere. Cirklernes størrelse indikerer befolkningsstørrelse.",
+       x = "Incidens den 1. december",
+       y = "Incidens diff. ml. d. 24. nov. og 1. dec.",
+       caption = "@Straubinger | Data: SSI") +
   theme(plot.title = element_text(face = "bold", size = 16),
         plot.subtitle = element_text(size = 12),
         plot.caption = element_text(color = "#606F7B", margin = margin(t = 10)),
-        panel.grid = element_blank(),
-        axis.ticks.x = element_line(color = "#606F7B"),
-        legend.position = "bottom",
-        legend.title = element_blank(),
-        axis.title = element_blank(),
+        axis.title.x = element_text(color = "gray25", margin = margin(t = 8)),
+        axis.title.y = element_text(color = "gray25", margin = margin(r = 8)),
+        legend.position = "none",
+        panel.grid.minor = element_blank(),
         text = element_text(family = "Roboto"))
 
-ggsave(width = 8, height = 14, here("plot_corona_heat.png"), type = "cairo-png", dpi = 600)
+ggsave(width = width, height = height, here("plot_corona_scatter.png"), type = "cairo-png", dpi = 600)
+  
 
 
 
